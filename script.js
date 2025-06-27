2c0a410501cd200f572cdfffc4cda86bd9d23d01
Gabriel-cyber231 <73710798+Gabriel-cyber231@users.noreply.github.com>
2025-06-27 11:39:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 function login() {
-    const username = document.getElementById("username").value.trim().toLowerCase();
-    const password = document.getElementById("password").value.trim();
-    const error = document.getElementById("login-error");
-  
-    if (username === "admin" && password === "1234") {
-      document.getElementById("login-container").style.display = "none";
-      document.getElementById("main-container").style.display = "block";
-      fetchDollar();
-    } else {
-      error.textContent = "Usuario o contraseña incorrectos.";
-    }
+  const secret = document.getElementById("secret").value.trim().toLowerCase();
+  const error = document.getElementById("login-error");
+
+  if (secret === "dolarblue") {
+    document.getElementById("login-container").style.display = "none";
+    document.getElementById("main-container").style.display = "block";
+    fetchDollar();
+  } else {
+    error.textContent = "Palabra secreta incorrecta.";
   }
-  
-  
-  function logout() {
-    location.reload();
-  }
-  
-  function fetchDollar() {
-    fetch("https://api.bluelytics.com.ar/v2/latest")
-      .then(response => response.json())
-      .then(data => {
-        const blue = data.blue;
-        document.getElementById("blue-buy").textContent = blue.value_buy;
-        document.getElementById("blue-sell").textContent = blue.value_sell;
-      })
-      .catch(err => {
-        document.getElementById("main-container").innerHTML = "<p>Error al obtener datos del dólar.</p>";
-      });
-  }
+}
+
+function logout() {
+  location.reload();
+}
+
+function fetchDollar() {
+  fetch("https://api.bluelytics.com.ar/v2/latest")
+    .then(response => response.json())
+    .then(data => {
+      const blue = data.blue;
+      document.getElementById("blue-buy").textContent = blue.value_buy;
+      document.getElementById("blue-sell").textContent = blue.value_sell;
+    })
+    .catch(err => {
+      document.getElementById("main-container").innerHTML = "<p>Error al obtener datos del dólar.</p>";
+    });
+}
+
   
